8789f8dac9178ff3f892b79295b3e257948895f8
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-06-06 16:36:17 +0100

    Created Commons module and moved to fsx scripts

---
+++ Euler Project/Commons.fsx
@@ -0,0 +1,11 @@
+module Commons
+
+let primeFactors n = 
+    let rec getFactors prime acc (num:int64) = 
+        if prime = num then
+            prime::acc
+        elif num % prime = 0L then 
+            getFactors prime (prime::acc) (num/prime)
+        else
+            getFactors  (prime+1L) acc num
+    getFactors 2L [] n
---
+++ Euler Project/Prob003.fsx
@@ -0,0 +1,13 @@
+module Prob003
+
+#load "Commons.fsx"
+open Commons
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+//
+// What is the largest prime factor of the number 600851475143 ?
+
+let solution num =
+    primeFactors num
+    |> List.sortDescending
+    |> List.take 1    
