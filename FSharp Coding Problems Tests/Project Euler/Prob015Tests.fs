d957b7d06ca92d5072f2e9479673d3f68cd6a564
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-09-06 16:38:38 +0100

    Initial implementation and tests for Prob015

---
+++ FSharp Coding Problems Tests/Project Euler/Prob015Tests.fs
@@ -0,0 +1,21 @@
+﻿module Prob015Tests
+
+open Xunit
+open FsUnit.Xunit
+
+open PE_Prob015
+
+[<Fact>]
+let ``valid paths for a 1x1 grid SHOULD be 2`` () =
+    solution 1
+    |> should equal 2
+
+[<Fact>]
+let ``valid paths for a 2x2 grid SHOULD be 6`` () =
+    solution 2
+    |> should equal 6
+
+[<Fact>]
+let ``valid paths for a 3x3 grid SHOULD be 20`` () =
+    solution 3
+    |> should equal 20
--- FSharp Coding Problems/Project Euler/Prob015.fs
+++ FSharp Coding Problems/Project Euler/Prob015.fs
@@ -1,5 +1,23 @@
-﻿module Prob015
+﻿module PE_Prob015
 
 //Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
 //How many such routes are there through a 20×20 grid?
+
+// 1x1 -> v(04) -> 2
+// 2x2 -> v(09) -> 6
+// 3x3 -> v(16) -> 22
+
+// f(n) = f(n-1) + n*2
+
+//  +--+--+--+
+//  +--+--+--+
+//  +--+--+--+
+//  +--+--+--+
+
+let addLayer prev n =
+    prev + n*2
+
+let solution n =
+    [2..n]
+    |> List.fold addLayer 2
