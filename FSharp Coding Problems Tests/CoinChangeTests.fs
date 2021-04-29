588835b2b655e0c5fde83e9f8fedb254d918fa0e
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2021-04-29 08:12:02 +0200

    coin change - first approach

--- FSharp Coding Problems Tests/CoinChangeTests.fs
+++ FSharp Coding Problems Tests/CoinChangeTests.fs
@@ -9,10 +9,15 @@ open CoinChange
 
 type OneCoin = 
     static member x()=
-        Gen.elements CoinSizes
+        Gen.elements standardCoinSizes
         |> Arb.fromGen
 
 [<Property(Arbitrary=[| typeof<OneCoin> |])>]
 let ``change for one of the available sizes SHOULD return one elem of the size`` (coin:int) =
     change coin
     |> should equal [coin]
+
+[<Fact>]
+let ``change for 3 SHOULD return 1 and 2`` ()=
+    change 3
+    |> should matchList [1;2]
--- FSharp Coding Problems/CoinChange.fs
+++ FSharp Coding Problems/CoinChange.fs
@@ -1,13 +1,32 @@
 module CoinChange
 
-let CoinSizes = [1; 2; 5; 10; 50]
+let standardCoinSizes = [1; 2; 5; 10; 50]
+
+// array[1...amount] of list of coins
+// iterate over coinSizes to add the new coin
+
+let private loop (array:int list []) coin = 
+    let createCouples coin amn l =
+        if amn < coin
+        then [l]
+        else [l; coin::array.[amn-coin]]
+    
+    array
+    |> Array.mapi (createCouples coin)
+    |> Array.map (fun ill -> ill |> List.filter (fun l-> l.Length > 0) |> List.minBy List.length)
 
 let customChange (sizes:int list) (amount:int) : int list =
     let found = 
         sizes
         |> List.tryFind (fun s -> s = amount)
+
     match found with
     | Some(_) -> [amount]
-    | None -> []
+    | None ->
+        let init = Array.create (amount+1) []
+        let state = 
+            standardCoinSizes
+            |> List.fold loop init
+        state.[amount]
 
-let change amount = customChange CoinSizes amount
+let change amount = customChange standardCoinSizes amount
